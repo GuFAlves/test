742f528fe664f118223f9a910f26b1225b761570
Gustavo Fagundes Alves <gustavo.alves@mundiallogistics.com.br>
2026-08-11 11:06:21 -0300

    Cria automação Inicial

---
+++ automacao.py
@@ -0,0 +1,16 @@
+
+print("Inicio da Automação")
+
+pedidos = ["p1","p2","p3","p4","p5"]
+
+print("Inicio do Processamento de pedidos")
+
+class ProcessarPedido:
+    def __init__(self, pedidos):
+        self.pedidos
+
+for pedidos in range(5):
+    print("Imprimindo Pedido")
+
+
+print("Processo Finalizado")
