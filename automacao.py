10189ba8720e131a2f208b23015adf4945bd3137
Gustavo Fagundes Alves <gustavo.alves@mundiallogistics.com.br>
2026-08-11 15:39:38 -0300

    Aumenta processamento para 10 pedidos

--- automacao.py
+++ automacao.py
@@ -1,7 +1,7 @@
 
 print("Inicio da Automação")
 
-pedidos = ["p1","p2","p3","p4","p5"]
+pedidos = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10"]
 
 print("Inicio do Processamento de pedidos")
 
@@ -9,7 +9,7 @@ class ProcessarPedido:
     def __init__(self, pedidos):
         self.pedidos
 
-for pedidos in range(5):
+for pedidos in range(10):
     print("Imprimindo Pedido")
 
 
